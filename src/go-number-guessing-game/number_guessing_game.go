e81a417aef56edeec6eacf100df52eb4d7af76f9
Samuel Orchestrator Williams <samuelowilliams.dev@gmail.com>
2025-08-20 09:18:31 +0200

    feat: Implemented displayStartUpMessage function

--- src/go-number-guessing-game/number_guessing_game.go
+++ src/go-number-guessing-game/number_guessing_game.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
-func main() {
-	print := fmt.Println
+func displayStartUpMessage() {
+	fmt.Println("Welcome to the Number Guessing Game!")
+	fmt.Println("I'm thinking of a number between 1 and 100.")
+	fmt.Println("Select game difficaulty to begin.")
+}
 
-	print("Number Guessing Game init")
+func startGame() { displayStartUpMessage() }
+func main() {
+	startGame()
 }
